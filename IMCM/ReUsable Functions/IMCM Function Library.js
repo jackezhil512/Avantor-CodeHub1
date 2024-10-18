48ae443d96e17ab25010ca95ca39c27b6c69b5e1
Avantor-CodeHub <Avantor-CodeHub>
2024-10-18 09:33:42 +0000

    DS3 - [adding IMCM/ReUsable Functions/IMCM Function Library.js]

--- IMCM/ReUsable Functions/IMCM Function Library.js
+++ IMCM/ReUsable Functions/IMCM Function Library.js
@@ -108,11 +108,11 @@ function SelectVendorStockroomAndEmailInInterations(Vendor,Stockroom,Email){
 setShadowDOM(true);
 wait(1000);
 click(fallback(`byXPath("//span[contains(text(),'All Vendors')]")`));
-click(fallback(`byXPath('//*[text()="${Vendor}"]')`));
+click(fallback(`byXPath("//span[contains(text(),"${Vendor}"]")`));
 click(fallback(`byXPath("//span[contains(text(),'All Stockrooms')]")`));
-click(fallback(`byXPath('//*[text()="${Stockroom}"]')`));
+click(fallback(`byXPath("//span[contains(text(),"${Stockroom}"]")`));
 click(fallback(`byXPath("//span[contains(text(),'All Emails')]")`));
-click(fallback(`byXPath('//*[text()="${Email}"]')`));
+click(fallback(`byXPath("//span[contains(text(),"${Email}"]")`));
 }
 
 

1f73702e9c7d88b7d8777642cb30b22308a402cf
Avantor-CodeHub <Avantor-CodeHub>
2024-10-21 09:09:43 +0000

    DS3 - [adding Estorefront/E-Storefront Regression/Reusable Function library/ReusableFunction.js]

--- Estorefront/E-Storefront Regression/Reusable Function library/ReusableFunction.js
+++ Estorefront/E-Storefront Regression/Reusable Function library/ReusableFunction.js
@@ -714,7 +714,7 @@ setValue(fallback(`textbox("NEW_LIST")`,), NewShoppingList);
 
 function NegativeSceanrio_withoutQtyAdditem_in_newshoppinglist_pdp(){
 
- // setVariablesIfNeeded('{ds}/../../Test Data/InputSheet_Regreesion.csv','HashDPL',0,'en_US');
+  setVariablesIfNeeded('{ds}/../../Test Data/InputSheet_Regreesion.csv','HashDPL',0,'en_US');
    setShadowDOM(true);
   
   //click(fallback(`button("+")`));

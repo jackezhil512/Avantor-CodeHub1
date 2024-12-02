9983872a706883306a795e618c4adb9363ad5228
Avantor-CodeHub <Avantor-CodeHub>
2024-12-02 07:24:32 +0000

    DS3 - [adding IMCM/ReUsable Functions/IMCM Function Library.js]

--- IMCM/ReUsable Functions/IMCM Function Library.js
+++ IMCM/ReUsable Functions/IMCM Function Library.js
@@ -1385,7 +1385,7 @@ assertExists(fallback(`byXPath('//*[text()=" You have successfully deleted integ
 function createALLIntegrationIfNotExist(){
 let value = _isVisible(fallback(`byXPath("//img[@src='/assets/icons/ellipsis-icon.svg']/parent::span")`));
 log(value);
-if(true == value){
+if(false == value){
  AddALLintegrationWithText()
 }
 }

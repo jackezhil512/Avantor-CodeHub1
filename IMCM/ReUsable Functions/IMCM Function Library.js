3921dc48cb2a37a55de3f46ccbb42165b91d9bfe
Avantor-CodeHub <Avantor-CodeHub>
2024-12-02 07:24:32 +0000

    DS3 - [adding IMCM/ReUsable Functions/IMCM Function Library.js]

--- IMCM/ReUsable Functions/IMCM Function Library.js
+++ IMCM/ReUsable Functions/IMCM Function Library.js
@@ -1485,3 +1485,96 @@ click(fallback(`byXPath("//span[contains(text(),'Save')]/ancestor::button")`));
 assertExists(fallback(`byXPath('//*[text()=" Integration created successfully "]')`));
 }
 
+
+function CreateVendorAndAddIntegration(){
+   
+wait(5000);
+var RandomVal=eval(" Math.floor(Math.random()*1000)");
+var Date=datefunc();
+log(Date);
+var VendorName="Automation" +RandomVal;
+log(VendorName);
+var StockRoomName=$ConsumptionStockroom;
+log(StockRoomName);
+var Email="Automation"+RandomVal+"@gmail.com";
+setDescription("Vendor Maintenance");
+NavigateToVendorMaintenancePage();
+wait(1000);
+setDescription("Click Add new vendor");
+click(fallback(`byXPath('//*[text()=" Add New Vendor "]')`));
+setDescription("Set vender Naame");
+setValue(fallback(`byXPath('//*[text() =" Create New Vendor "]/following::input[1]')`),VendorName);
+setValue(fallback(`byXPath('//*[text() =" Create New Vendor "]/following::input[4]')`),RandomVal);
+setDescription("Click next");
+click(fallback(`custom("mat-icon", "arrow_forward")`));
+setDescription("Enter vendor name");
+assertExists(fallback(`byXPath('//*[text()="${VendorName }"]')`));
+setDescription("Validate PDF OUTPUT");
+assertExists(fallback(`byXPath('//*[text()="PDF OUTPUT"]')`));
+setDescription("Click next");
+click(fallback(`custom("mat-icon", "arrow_forward")`));
+setDescription("Validate Vendor Name");
+assertExists(fallback(`byXPath('//*[text()="${VendorName }"]')`));
+setDescription("Validate out Put type");
+assertExists(fallback(`cell("PDF OUTPUT")`));
+setDescription("Click on Save and Add Vendor");
+click(fallback(`span("Save & Add Vendor[1]")`));
+setDescription("Validate Success Message");
+assertExists(fallback(`div("VENDOR CREATED SUCCESSFULLY[9]").xy(0.41, 0.32 )`));
+wait(1000);
+NavigateToIntegrationmaintenance();
+click(fallback(`byXPath("//span[contains(text(),'ADD NEW INTEGRATION')]/ancestor::button")`));
+click(fallback(`byXPath("(//div[contains(text(),'Vendor')]/span/following::mat-select)[1]")`));
+click(fallback(`byXPath("//span[contains(text(),'{VendorName}') and @class='vendor-option']")`,
+   `byXPath("//span[contains(text(),'${VendorName}') and @class='vendor-option']")`));
+wait(2000);
+setValue(fallback(`byXPath("//h4[contains(text(),'Add New Integration')]")`),"{{TAB}}");
+wait(2000);
+click(fallback(`byXPath("(//div[contains(text(),'Stockroom')]/span/following::mat-select)[1]")`));
+click(fallback(`byXPath('//*[text()="${StockRoomName}"]')`,
+   `byXPath('//*[text()='{StockRoomName}']')`,
+   `byXPath('//*[text()='${StockRoomName}']')`,
+   `byXPath('//*[text()="Automation Regular Stockroom"]')`,
+   `span(1, _in(custom("mat-option", "mat-option-155")))`,
+   `span(1, _in(custom("mat-option", {'id':'mat-option-155'})))`,
+   `span("vendor-option[1]")`,
+   `span("Automation Regular Stockroom[1]")`,
+   `byXPath("//mat-option[@id='mat-option-155']/span/span")`,
+   `byXPath('/html/body/div[2]/div[4]/div/div/div/div[2]/mat-option[2]/span/span')`,
+   `byXPath("id('mat-option-155')/span[@class='mat-option-text']/span[@class='vendor-option']")`,
+   `byJQuery('div[class="ng-tns-c89-179 ng-trigger ng-trigger-transformPanel mat-select-panel mat-primary"] > div[class="ng-tns-c89-179"] > mat-option[class="mat-option mat-focus-indicator mat-option-multiple ng-star-inserted"] > span[class="mat-option-text"] > span[class="vendor-option"]')`,
+   `byJQuery('#mat-option-155 > .mat-option-text > .vendor-option')`));
+wait(2000);
+setValue(fallback(`byXPath("//h4[contains(text(),'Add New Integration')]")`),"{{TAB}}");
+wait(2000);
+click(fallback(`byXPath("(//div[contains(text(),'Output Type')]/span/following::mat-select)[1]")`));
+click(fallback(`byXPath("//span[contains(text(),'PDF OUTPUT')]/parent::span")`));
+click(fallback(`byXPath("//span[contains(text(),'Next')]/parent::span")`,
+   `byXPath("//span[contains(text(),'Next')]/parent::span")`));
+wait(2000);
+click(fallback(`byXPath("//span[contains(text(),'Request Integration')]/ancestor::label")`));
+wait(2000);
+click(fallback(`byXPath("(//label[contains(text(),'Primary Email')]/following::div[contains(text(),'Add email')])[1]")`));
+wait(2000);
+var TempEmail=eval("'Auto'+ Math.floor(Math.random()*10000) + '@gmail.com'");
+log(TempEmail);
+setValue(fallback(`byXPath("(//label[contains(text(),'Primary Email')]/following::input)[1]")`),TempEmail);
+click(fallback(`byXPath("//mat-icon[text()='save']")`));
+click(fallback(`byXPath("//span[contains(text(),'Next')]/parent::span")`,
+   `byXPath("//span[contains(text(),'Next')]/parent::span")`));
+click(fallback(`byXPath("//span[contains(text(),'Save')]/parent::span")`));
+wait(2000);
+setDescription("Get Vendor Name");
+var VendorNameInFirstRow=getText(byXPath("(//td[contains(@class,'mat-column-vendorData')])[1]"));
+setDescription("Get Stockroom Name");
+var StockroomNameFirstRow=getText(byXPath("(//td[contains(@class,'mat-column-stockroomData')])[1]"));
+setDescription("verify updfated and searched vendor results are same or not?");
+assertEqual(fallback(`VendorNameInFirstRow`,
+   `VendorName`),VendorName);
+setDescription("verify updfated and searched stockroom results are same or not?");
+assertEqual(fallback(`StockroomNameFirstRow`,
+   `StockroomName`),StockRoomName);
+
+}
+
+

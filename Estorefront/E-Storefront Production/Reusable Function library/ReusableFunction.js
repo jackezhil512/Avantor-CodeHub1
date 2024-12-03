1dad8eeba594194d14b110e5ba849241631434f3
Avantor-CodeHub <Avantor-CodeHub>
2024-12-03 15:17:05 +0000

    DS3 - [adding Estorefront/E-Storefront Production/Reusable Function library/ReusableFunction.js]

--- Estorefront/E-Storefront Production/Reusable Function library/ReusableFunction.js
+++ Estorefront/E-Storefront Production/Reusable Function library/ReusableFunction.js
@@ -1062,7 +1062,7 @@ function VerifyRetunbuttonInOrderdetaispage(){
 
 }
 
-function placeorder_Prod(){
+function placeorderProd(){
 
   setVariablesIfNeeded('{ds}/../../Test Data/InputSheet_Regreesion.csv','HashDPL',0,'en_US');
 	setShadowDOM(true);

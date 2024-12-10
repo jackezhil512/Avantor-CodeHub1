f7484db12c7255c4c31897317759d7dcbec93bd3
Avantor-CodeHub <Avantor-CodeHub>
2024-12-10 13:09:27 +0000

    DS3 - [adding Estorefront/E-Storefront Regression/Reusable Function library/ReusableFunction.js]

--- Estorefront/E-Storefront Regression/Reusable Function library/ReusableFunction.js
+++ Estorefront/E-Storefront Regression/Reusable Function library/ReusableFunction.js
@@ -1233,12 +1233,13 @@ function GlobalwebclearCart(){
   //assertExists(fallback(`listItem("Successfully cleared your cart")`));
 //click(fallback(`div("miniCartContent")`));
 wait(50000)
- let value = _isVisible(fallback(`link(0, _in(div("miniCartContent")))`));
+ let value = _isVisible(fallback(`link("0  items in your cart")`));
     log(value);
 if (true == value){
   wait(5000)
  // if (isEnabled(submit("View My Cart"))){
-    click(fallback(`link(0, _in(div("miniCartContent")))`));
+    //click(fallback(`link(0, _in(div("miniCartContent")))`));
+    click(fallback(`link("0  items in your cart")`));
     wait(5000)
   assertExists(fallback(`strong("Your Cart is Empty")`));
    

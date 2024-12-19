d1a05a2ca260ed022243999c6bc7cca25de9798a
Muralikrishna-Gatta_avtr <Muralikrishna-Gatta_avtr>
2024-12-19 15:18:25 +0000

    DS3 - [adding Estorefront/E-Storefront Production/Reusable Function library/ReusableFunction.js]

--- Estorefront/E-Storefront Production/Reusable Function library/ReusableFunction.js
+++ Estorefront/E-Storefront Production/Reusable Function library/ReusableFunction.js
@@ -489,7 +489,7 @@ click(fallback(`submit("Add To Cart")`));
 assertExists(fallback(`heading3("Cart Summary")`));
 
 var CatlogNumberInCartpage = getText((`link("cart-catalog-link-color cx-link")`));
-log CatlogNumberInCartpage
+log (CatlogNumberInCartpage);
 //assertExists(fallback(`link("414004-315[1]")`));  
 //assertExists(fallback(`link("414004-146[1]")`));  
 }

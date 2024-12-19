3de2400c58d365f728ec8d4b189791400445dbcc
Harish-Rao_avtr <Harish-Rao_avtr>
2024-12-19 15:38:25 +0000

    Appvance Commit [skip ci] - [adding IMCM/ReUsable Functions/IMCM Function Library.js]

--- IMCM/ReUsable Functions/IMCM Function Library.js
+++ IMCM/ReUsable Functions/IMCM Function Library.js
@@ -1616,6 +1616,8 @@ setValue(fallback(`byXPath("(//label[contains(text(),'Primary Email')]/following
    `byXPath("(//label[contains(text(),'Primary Email')]/following::input)[1]")`),TempEmail);
 click(fallback(`byXPath("//mat-icon[text()='save']")`));
    wait(2000);
+click(fallback(`byXPath("//div[@formarrayname='requestCCEmails']//span[text()='add']")`));
+
 setValue(fallback(`byXPath("(//label[contains(text(),'CC Email')]/following::input)[1]")`,
    `byXPath("(//label[contains(text(),'CC Emaill')]/following::input)[1]")`,
    `byXPath("(//label[contains(text(),'CC Email')]/following::input)[1]")`),TempEmail1);
@@ -1628,10 +1630,26 @@ wait(2000);
 setValue(fallback(`byXPath("(//span[contains(text(),'Order Integration') and @class='mat-checkbox-label']/following::input)[1]")`),TempEmail1);
 click(fallback(`byXPath("//mat-icon[text()='save']")`));
 wait(2000);
+   wait(2000);
+click(fallback(`byXPath("//div[@formarrayname='requestCCEmails']//span[text()='add']")`));
+
+setValue(fallback(`byXPath("(//label[contains(text(),'CC Email')]/following::input)[1]")`,
+   `byXPath("(//label[contains(text(),'CC Emaill')]/following::input)[1]")`,
+   `byXPath("(//label[contains(text(),'CC Email')]/following::input)[1]")`),TempEmail2);
+click(fallback(`byXPath("//mat-icon[text()='save']")`));
+
+
 click(fallback(`byXPath("//span[contains(text(),'Receiving Integration') and @class='mat-checkbox-label']")`));
 click(fallback(`byXPath("(//span[contains(text(),'Receiving Integration') and @class='mat-checkbox-label']/following::div[contains(text(),'Add email')])[1]")`));
 wait(2000);
-setValue(fallback(`byXPath("(//span[contains(text(),'Receiving Integration') and @class='mat-checkbox-label']/following::input)[1]")`),TempEmail2);
+setValue(fallback(`byXPath("(//span[contains(text(),'Receiving Integration') and @class='mat-checkbox-label']/following::input)[1]")`),TempEmail1);
+click(fallback(`byXPath("//mat-icon[text()='save']")`));
+   wait(2000);
+click(fallback(`byXPath("//div[@formarrayname='requestCCEmails']//span[text()='add']")`));
+
+setValue(fallback(`byXPath("(//label[contains(text(),'CC Email')]/following::input)[1]")`,
+   `byXPath("(//label[contains(text(),'CC Emaill')]/following::input)[1]")`,
+   `byXPath("(//label[contains(text(),'CC Email')]/following::input)[1]")`),TempEmail);
 click(fallback(`byXPath("//mat-icon[text()='save']")`));
 wait(4000);
 click(fallback(`byXPath("(//span[contains(text(),'Next')]/ancestor::button)[2]")`));
